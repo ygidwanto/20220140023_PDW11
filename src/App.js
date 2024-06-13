3d0f8cdad3ef159dff8085c1a3a4898d6ee68c21
Yogi Dewanto <yogi.dewanto.ft22@mail.umy.ac.id>
2024-06-13 09:48:10 +0700

    saveall

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src= "https://cdn.idntimes.com/content-images/community/2021/07/222038363-1410421169293913-657210553370221352-n-07a4f29dba4b014075e472810bb49bf0-e14fc3a7d17e1702a12c04660f6910e5_600x400.jpg" className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Gambar <code>Muria</code> dan Jagalah kebersihan.
         </p>
         <a
           className="App-link"
